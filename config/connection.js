433267d88052b0a56fd9a7de9b8d9b720f5c6352
mralj46 <alj_thebest@yahoo.com>
2018-05-31 21:05:35 -0400

    update

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,17 @@ var mysql = require('mysql');
 var connection;
 
 if (process.env.JAWSDB_URL) {
-	connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'burgers_db1'
-  });
+    connection = mysql.createConnection({
+        host: 'k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'wfdsd0fe4x66jk1z',
+        password: 'po7bd92fbd4mrv5z',
+        database: 'wcrm5r6hrwmsnpkw'
+    });
 };
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
         return;
